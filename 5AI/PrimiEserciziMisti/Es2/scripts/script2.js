a2454d18abbd8930a8ce264066767a910d1e5eb2
Nicolò Buganza <nicolo.buganza@tiscali.it>
2024-12-10 08:38:29 +0100

    5AI intro ex
    more to come....

---
+++ 5AI/PrimiEserciziMisti/Es2/scripts/script2.js
@@ -0,0 +1,2 @@
+let h1 = document.getElementById("titolo");
+h1.textContent = "Titolo aggiornato!";
---
+++ 5AI/PrimiEserciziMisti/Es3/scripts/script3.js
@@ -0,0 +1,13 @@
+let ul = document.getElementById("lista");
+
+let li1 = document.createElement("li");
+li1.textContent = "Item 1";
+ul.appendChild(li1);
+
+let li2 = document.createElement("li");
+li2.textContent = "Item 2";
+ul.appendChild(li2);
+
+let li3 = document.createElement("li");
+li3.textContent = "Item 3";
+ul.appendChild(li3);
