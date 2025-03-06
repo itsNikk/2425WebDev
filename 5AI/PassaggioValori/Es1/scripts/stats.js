35da4745dcd77c7fbc3be27287359ff9bfc78b33
itsNikk <nicolo.buganza@tiscali.it>
2025-03-06 11:14:32 +0100

    Create stats.js

---
+++ 5AI/PassaggioValori/Es1/scripts/stats.js
@@ -0,0 +1,57 @@
+const selectedNumbers = JSON.parse(localStorage.getItem('selectedNumbers'));
+
+if (selectedNumbers.length === 0) {
+    alert("Non hai selezionato alcun numero.");
+    window.location.href = 'es1Index.html';
+}
+
+// Calcolare le statistiche
+const total = selectedNumbers.length;
+const sum = computeSum(selectedNumbers)
+const average = sum / total;
+const min = findMin(selectedNumbers)
+const max = findMax(selectedNumbers)
+
+
+document.getElementById('total').textContent = total;
+document.getElementById('sum').textContent = sum;
+//toFixed(int) prende solo 'int' numeri dopo la virgola
+document.getElementById('average').textContent = average.toFixed(2);
+document.getElementById('min').textContent = min;
+document.getElementById('max').textContent = max;
+
+// Gestire il pulsante "Torna Indietro"
+document.getElementById('backBtn').addEventListener('click', () => {
+    window.location.href = 'es1Index.html';
+});
+
+function computeSum(arr) {
+    let sum = 0;
+    for (let elem of arr) {
+        sum += parseInt(elem);
+    }
+    return sum;
+}
+
+function findMin(arr) {
+    let currentMin = arr[0];
+    for (let elem of arr) {
+        if (elem < currentMin) {
+            currentMin = elem;
+        }
+
+    }
+
+    return currentMin;
+}
+
+function findMax(arr) {
+    let currentMax = arr[0];
+    for (let elem of arr) {
+        if (elem > currentMax) {
+            currentMax = elem;
+        }
+    }
+
+    return currentMax;
+}
