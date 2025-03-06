cace942a58206872cbd387739d29d75ef420d432
itsNikk <nicolo.buganza@tiscali.it>
2025-03-06 11:15:16 +0100

    Update index.js

--- 5AI/PassaggioValori/Es1/scripts/index.js
+++ 5AI/PassaggioValori/Es1/scripts/index.js
@@ -4,7 +4,6 @@ const goToStatsButton = document.getElementById('toStatsBtn');
 
 let selectedNumbers = [];
 
-// Funzione per creare i bottoni
 for (let i = 1; i <= numButtons; i++) {
     const button = document.createElement('button');
     button.textContent = i;
@@ -14,7 +13,6 @@ for (let i = 1; i <= numButtons; i++) {
     buttonsContainer.appendChild(button);
 }
 
-// Gestione del pulsante per andare alla pagina delle statistiche
 goToStatsButton.addEventListener('click', () => {
     localStorage.setItem('selectedNumbers', JSON.stringify(selectedNumbers));
     window.location.href = 'stats.html';
